b5e98bd01c9bc57e54829c0af96ce711a6cdd606
Steeve Matou <steeve.matou@gmail.com>
2022-09-17 09:19:41 +0200

    first add js

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 function emailValidation() {
   const inputEmail = document.querySelector(".email");
-  let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  const mailformat = new RegExp(
+    /^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/,
+    "gm"
+  );
   const errorAlert = document.querySelector(".form__error");
   console.log(errorAlert);
 
   console.log(inputEmail);
   inputEmail.addEventListener("input", (e) => {
     console.log(e.target.value);
-    if (e.target.value == mailFormat) {
+    if (e.target.value.match.mailFormat) {
       return "";
+    } else {
+      errorAlert.style.opacity = "1";
     }
   });
 }
